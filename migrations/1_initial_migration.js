9138f89ebe2abf4f6c814ff93efac1b0f475bc89
Karen1424 <vip.karengrigoryan@mail.ru>
2022-05-12 18:01:38 +0400

    NFT-MARKETPLACE DEMO VERSION

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,12 @@
+const Migrations = artifacts.require("Migrations");
+const NFT =  artifacts.require("NFT");
+const Market = artifacts.require("Market");
+
+
+module.exports = function (deployer) {
+
+  deployer.deploy(Migrations);
+  deployer.deploy(NFT);
+  deployer.deploy(Market);
+
+};
